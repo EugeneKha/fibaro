04bd27809f1cb9388f0b2072f1b8764a9a4bec1a
Eugene Khasenevich <khasenevich@Yauhens-MacBook-Pro.local>
2017-06-27 22:19:38 +0300

    Init

---
+++ gvs.lua
@@ -0,0 +1,117 @@
+--[[
+%% properties
+31 value
+%% events
+%% globals
+--]]
+
+function water_t()
+	return tonumber(fibaro:getValue(31, "value"))
+end
+
+function freon_t()
+	return tonumber(fibaro:getValue(32, "value"))
+end
+
+function turnOn_hitting()
+    fibaro:sleep(1000)
+    if (tonumber(fibaro:getValue(37, "value")) == 0)
+    then
+	    fibaro:call(37, "turnOn")
+        fibaro:debug('Тепловой насос включен')
+    end
+end
+
+function turnOff_hitting()
+    fibaro:sleep(1000)
+    if (tonumber(fibaro:getValue(37, "value")) == 1)
+    then
+	    fibaro:call(37, "turnOff")
+        fibaro:debug('Тепловой насос вЫключен')
+    end
+end
+
+function turnOn_pump()
+    fibaro:sleep(1000)
+    if (tonumber(fibaro:getValue(38, "value")) == 0)
+    then
+	    fibaro:call(38, "turnOn")
+        fibaro:debug('Насос ГВС включен')
+    end
+end
+
+function turnOff_pump()
+    fibaro:sleep(1000)
+    if (tonumber(fibaro:getValue(38, "value")) == 1)
+    then
+        fibaro:call(38, "turnOff")
+        fibaro:debug('Насос ГВС вЫключен')
+    end
+end
+
+function start_hitting()
+
+    fibaro:debug('Начат нагрев воды')
+
+
+    while true do
+
+        if (water_t() >= 45)
+        then
+            fibaro:debug('Нагрев воды закончен')
+            break
+        end
+
+        fibaro:debug('Нагрев воды продолжается ' .. water_t())
+
+        turnOn_pump()
+
+        if (freon_t() >= 57)
+        then
+            turnOff_hitting()
+        end
+
+        if (freon_t() < 57)
+        then
+            turnOn_hitting()
+        end  
+
+        fibaro:sleep(5000) -- Run every 5 SECOND
+    end
+
+    turnOff_pump()
+    turnOff_hitting()
+
+end
+
+function need_hitting()
+    fibaro:debug('Тест воды ГВС начат')
+
+    for i=0,10,1 
+    do
+        if (water_t() > 30)
+        then
+            break
+        end
+        turnOn_pump()
+        fibaro:sleep(30*1000)
+    end
+    turnOff_pump()
+    turnOff_hitting()
+
+
+    fibaro:debug('Тест воды ГВС '..water_t())
+    return water_t() < 30
+end
+
+turnOff_hitting()
+turnOff_pump()
+
+while true do
+    if (need_hitting())
+    then
+        start_hitting()
+    end
+    fibaro:sleep(30*60*1000) -- sleep for 30 minutes
+end
+
